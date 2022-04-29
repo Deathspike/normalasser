67b2b9b0169d0407dd9a2780cde8c117eb05b645
Deathspike <meister.deathspike@outlook.com>
2022-04-29 19:16:21 +0200

    Update postinstall to target animesync

--- postinstall.js
+++ postinstall.js
@@ -1,20 +1,20 @@
 const fetch = require('node-fetch');
-const fs = require('fs-extra');
+const fs = require('fs');
 const path = require('path');
 const zlib = require('zlib');
-let apiUrl = 'https://api.github.com/repos/animeloyalty/animeloyalty-static/contents/';
-let rawUrl = 'https://github.com/animeloyalty/animeloyalty-static/raw/master/';
+let apiUrl = 'https://api.github.com/repos/Deathspike/animesync/contents/static/download/';
+let rawUrl = 'https://github.com/Deathspike/animesync/raw/master/static/download/';
 let staticPath = path.join(__dirname, 'static');
 
 async function checksumAsync(remotePath) {
-  return await fetch.default(new URL(path.dirname(remotePath), apiUrl))
+  return await fetch(new URL(path.dirname(remotePath), apiUrl).toString())
     .then(x => x.json())
     .then(x => x.find(x => x.name === path.basename(remotePath)).sha);
 }
 
 async function downloadAsync(localPath, remotePath) {
   return await new Promise((resolve, reject) => {
-    fetch.default(new URL(remotePath, rawUrl)).then((res) => res.body
+    fetch(new URL(remotePath, rawUrl).toString()).then((res) => res.body
       .pipe(zlib.createGunzip())
       .pipe(fs.createWriteStream(localPath))
       .on('error', reject)
@@ -24,21 +24,24 @@ async function downloadAsync(localPath, remotePath) {
 
 async function ffmpegAsync(localName, remotePath) {
   const hashPath = path.join(staticPath, `${localName}.cks`);
-  const localHash = await fs.readFile(hashPath, 'utf8').catch(() => {});
+  const localHash = await fs.promises.readFile(hashPath).then(String).catch(() => {});
   const remoteHash = await checksumAsync(remotePath);
   if (localHash !== remoteHash) {
-    await fs.ensureDir(staticPath);
+    await fs.promises.mkdir(staticPath, {recursive: true});
     await downloadAsync(path.join(staticPath, localName), remotePath);
-    await fs.chmod(path.join(staticPath, localName), '755');
-    await fs.writeFile(hashPath, remoteHash);
+    await fs.promises.chmod(path.join(staticPath, localName), '755');
+    await fs.promises.writeFile(hashPath, remoteHash);
   }
 }
 
 switch (process.platform) {
   case 'darwin':
-    ffmpegAsync('ffmpeg', 'ffmpeg/mac/ffmpeg.gz');
+    ffmpegAsync('ffmpeg', 'mac/ffmpeg.gz');
+    break;
+  case 'linux':
+    ffmpegAsync('ffmpeg', 'linux/ffmpeg.gz');
     break;
   case 'win32':
-    ffmpegAsync('ffmpeg.exe', 'ffmpeg/windows/ffmpeg.exe.gz');
+    ffmpegAsync('ffmpeg.exe', 'windows/ffmpeg.exe.gz');
     break;
 }
